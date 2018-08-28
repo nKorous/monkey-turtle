60e196595b8a42fa82362d9155eb6ae9a70413f4
Nate Korous <natekorous@workfront.com>
2018-08-27 18:02:58 -0600

    added semicolin to end...

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-let express = require('express')
-let cors = require('cors')
-let moment = require('moment')
-let dateFormat = require('dateformat')
-let bodyParser = require('body-parser')
+let express = require('express');
+let cors = require('cors');
+let moment = require('moment');
+let dateFormat = require('dateformat');
+let bodyParser = require('body-parser');
 
-let app = express()
+let app = express();
 
 
 //app.use declarations
 
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 //local vars
-let PORT = process.env.PORT || 9000
+let PORT = process.env.PORT || 9000;
 
 
 //App listens on ENV.PORT or 9000
 app.listen(PORT, () => {
 	console.log(`Listening on ${PORT}.`)
-})
+});
 
 app.get('/helloWorld', async (req, res) => {
 
-	let curDate = await getCurrentDate()
+	let curDate = await getCurrentDate();
 	res.status(200).send({"MSG": `Hello World`, "SENT": `MSG Sent on: ${curDate}`})
-})
+});
 
 
 
 //methods
 async function getCurrentDate(){
-	return dateFormat(moment(), 'yyyy-mm-dd HH:MM:ss')
-}
+	return dateFormat(moment(), 'yyyy-mm-dd HH:MM:ss');
+};
 
