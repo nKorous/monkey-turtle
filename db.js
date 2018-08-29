3929761c70dedb1f93437aaac247923bf873a857
Nate Korous <natekorous@workfront.com>
2018-08-29 11:05:02 -0600

    added start script

--- db.js
+++ db.js
@@ -6,17 +6,13 @@ const pg = new Client({
     ssl: process.env.IS_SSL
 })
 
-//"postgres://gykctjhvdcjcfj:5b2b1e6c99fe78cce12019acbf1fce5d70bd9fd1eb5630980f0df12c56de8c5a@ec2-54-235-160-57.compute-1.amazonaws.com:5432/dfpb7asu4h34un",
-
-console.log(process.env.DATABASE_URL)
-
-
 pg.connect(err => { 
         if(err){console.log(err)
         } else { 
             console.log('connected')} 
         })
 
+        /*
 let testMe = pg.query(`select * from users;`, (err, res) => {
     if(err){
         console.error(err)
@@ -26,10 +22,10 @@ let testMe = pg.query(`select * from users;`, (err, res) => {
         }
     }
 })
+*/
 
 let dbExports = {
-    pg,
-    testMe
+    pg
 }
 
 module.exports = dbExports
